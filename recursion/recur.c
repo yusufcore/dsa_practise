356d889ae5274ee14f92dab559196a9928b7ec0c
yusufcore <mohammadyusuf.work@gmail.com>
2026-07-14 14:11:31 +0000

    add notes of recusion

--- recursion/recur.c
+++ recursion/recur.c
@@ -11,3 +11,17 @@ int main(){
     fun(3);
     return 0;
 }
+/*Notes
+Recursion is technique where a function calls itself 
+It has 2 parts: base case and recursive case ---> 
+
+1) base case:  is the condition under which the function stops calling itself 
+2) recursive case:  is the condition under which the function calls itself 
+
+Recursion is traced using a tree structure
+
+Recursion uses a call stack to manage the function calls 
+
+Recursion can lead to stack overflow if the base case is never reached 
+
+Recursion is a memory consuming technique */
