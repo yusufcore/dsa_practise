b50ae2270c3f75cef9a4738af0b605f662f45e13
yusufcore <mohammadyusuf.work@gmail.com>
2026-07-22 14:32:28 +0000

    add array insertion

---
+++ array_adt/array_insertion.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Array{
+    int A[10];
+    int size;
+    int length;
+};
+
+void display(struct Array arr){
+    int i;
+    printf("\nElements are\n");
+    for(i=0;i<arr.length;i++){
+        printf("%d ", arr.A[i]);
+    }
+}
+
+void append(struct Array *arr, int x){
+    if(arr->length < arr->size){
+        arr->A[arr->length++] = x;
+    }
+}
+void insert(struct Array *arr, int index, int x){
+    if(index>=0 && index <=arr->length){
+        for(int i=arr->length; i>index; i--){
+            arr->A[i]= arr->A[i-1];
+        }
+        arr->A[index]=x;
+        arr->length++;
+    }
+}
+int main(){
+   
+    struct Array arr = {{2,3,4,5,6}, 10, 5};   // 10 is size of array, and 5 are the total elements contained in array.
+    insert(&arr, 0, 10);
+    display(arr);
+
+    return 0;
+
+}
