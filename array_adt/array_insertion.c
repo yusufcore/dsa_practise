0f0424c553f8a1ba55a17e674db1e1ed6814ff95
yusufcore <mohammadyusuf.work@gmail.com>
2026-07-23 03:26:59 +0000

    add comments

--- array_adt/array_insertion.c
+++ array_adt/array_insertion.c
@@ -6,7 +6,7 @@ struct Array{
     int length;
 };
 
-void display(struct Array arr){
+void display(struct Array arr){   // displaying the array
     int i;
     printf("\nElements are\n");
     for(i=0;i<arr.length;i++){
@@ -14,12 +14,12 @@ void display(struct Array arr){
     }
 }
 
-void append(struct Array *arr, int x){
+void append(struct Array *arr, int x){    // inserts the element to the last of the array.
     if(arr->length < arr->size){
         arr->A[arr->length++] = x;
     }
 }
-void insert(struct Array *arr, int index, int x){
+void insert(struct Array *arr, int index, int x){   // shifting and inserting
     if(index>=0 && index <=arr->length){
         for(int i=arr->length; i>index; i--){
             arr->A[i]= arr->A[i-1];
@@ -28,9 +28,9 @@ void insert(struct Array *arr, int index, int x){
         arr->length++;
     }
 }
-int main(){
+int main(){ 
    
-    struct Array arr = {{2,3,4,5,6}, 10, 5};   // 10 is size of array, and 5 are the total elements contained in array.
+    struct Array arr = {{2,3,4,5,6}, 10, 5};   // 10 is size of array, and 5 are the total elements contained in array(length).
     insert(&arr, 0, 10);
     display(arr);
 
