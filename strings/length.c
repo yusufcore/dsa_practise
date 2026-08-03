6eb0e407f59ce0bdf31b155d2393af22e9461d24
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-03 09:19:04 +0530

    add: find the length of a string in c.

---
+++ strings/length.c
@@ -0,0 +1,13 @@
+// find the length of a string.
+#include <stdio.h>
+
+int main(){
+    char ch[] = "welcome";
+    int i;
+    for(i = 0; ch[i]!='\0'; i++){
+
+    }
+    printf("Length of the string is: %d\n", i);
+
+    return 0;
+}
