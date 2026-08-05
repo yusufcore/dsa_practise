db18cc40b5925037da69fb8545ba017d1c70947c
yusufcore <mohammadyusuf.work@gmail.com>
2026-08-05 22:06:47 +0530

    add reversing string

---
+++ strings/reverse.c
@@ -0,0 +1,23 @@
+//reverse a string.
+#include <stdio.h>
+// Method 1 --> Using the same array.
+int main(){
+    char arr[] = {"python"};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    int i = 0;
+    int j = len -1;
+    int temp;
+    while(i < j){
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+    for(i = 0; i < len; i++){
+        printf("%c\t", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
